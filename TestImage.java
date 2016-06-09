43e1ba1f68be35869a82f4e875f0285e5eebc73f
ashley9314 <amtorr20@neiu.edu>
2016-06-08 23:32:07 -0500

    Add files via upload

---
+++ TestImage.java
@@ -0,0 +1,56 @@
+import java.awt.*;
+import javax.imageio.*;
+class TestImage {
+
+   public static void main(String args[])
+      throws Exception {
+      // Create a frame to display the image.
+      Frame frame = new Frame("Test Image");
+      frame.setSize(1024,768);
+      frame.setVisible(true);
+      Graphics gc = frame.getGraphics();
+      
+      try {
+         // Read the image from the file.
+          
+         Image img = new Image("Horse.jpg");
+         // Display the original image.
+         img.draw(gc,10,40);
+         
+         // Flip the image across y axis
+         img.flipY();
+         //write and draw flipped image
+         img.draw(gc,20+img.getWidth(),40);
+         img.write("HorseFlippedY.jpg");
+         
+         //change image to black and white
+         img.toBw();
+         // Display the black and white image.
+         img.draw(gc,20+img.getWidth(),40);
+         img.write("HorseBW.jpg");
+         
+         //change image intensity by giving percentage
+         img.chgInt(.5); 
+         // Display the black and white image.
+         img.draw(gc,20+img.getWidth(),40);
+         img.write("HorseInt.jpg");
+         
+         //rotate the image 90 degrees clockwise
+         img.rot90(); 
+         img.draw(gc,20+img.getWidth(),40);
+         img.write("HorseRot90.jpg");
+          
+         // Flip the image across y axis
+         img.flipX();
+         //write and draw flipped image
+         img.draw(gc,20+img.getWidth(),40);
+         img.write("HorseFlippedX.jpg");;
+        
+      } 
+      
+      catch (Exception e) 
+      {
+         System.out.println("Exception in main() "+e.toString());
+      }
+   }
+}
