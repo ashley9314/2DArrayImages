860b3107efbff32f534741d693a9a29f66f0a091
Rachel Trana <raxytrana@gmail.com>
2016-07-06 15:25:48 -0500

    Review and update by Rachel

--- Image.java
+++ Image.java
@@ -5,189 +5,119 @@ import java.util.*;
 import javax.imageio.*;
 import javax.imageio.stream.*;
 
-public class Image {
-
-
-   public int width,height; 
-   public int[][] pixels;
-   public int[][] newPixels;
-   public final static int WHITE = 0; 
-   public final static int BLACK = 255; 
-   public final static int MID_POINT = WHITE/2;   
-    
+public class Image 
+{
+	private int width, height; 
+	private int[][] pixels;
    
-   public Image(String filename)
-      throws Exception { read(filename); }
+	public Image(String filename) throws Exception 
+	{ 
+		this.read(filename); 
+	}
+	
+	public int getWidth()
+	{
+		return this.width;
+	}
+	
+	public int getHeight()
+	{
+		return this.height;
+	}
    
-   public int getWidth() 
-   { 
-      return width; 
+	public void read(String filename) throws Exception 
+	{
+		File fileImage = new File(filename);
+        
+        BufferedImage bufImage = ImageIO.read(fileImage);
+		this.width = bufImage.getWidth();
+		this.height = bufImage.getHeight();
+         
+        //complete the remainder of this method
+        this.pixels = new int[this.height][this.width];
+         
+        for (int row = 0; row < height; row++)
+        {
+        	for (int col = 0; col < width; col++)
+            {
+                this.pixels[row][col] = bufImage.getRGB(col, row);
+            }
+        }
    }
    
-   public int getHeight() 
-   { 
-      return height; 
-   }
+   private BufferedImage createBufferedImage() 
+   	{ 
+      	BufferedImage bufImage = new BufferedImage(this.width, this.height, BufferedImage.TYPE_INT_RGB);
+  
+      	for (int row = 0; row < this.height; row++)
+      	{
+         	for (int col = 0; col < this.width; col++)
+         	{
+         		bufImage.setRGB(col, row, this.pixels[row][col]);
+         	}
+        }
+         
+      	return bufImage;
+	}
    
-   public void read(String filename)
-      throws Exception {
-        
-         String ext = filename.substring
-         (filename.indexOf('.')+1);
-         
-         File fileImage = new File(filename);
-        
-         Iterator imageReaders = ImageIO.getImageReadersBySuffix(ext);
-         ImageReader imageReader;
-        
-         if (imageReaders.hasNext())
-            imageReader = (ImageReader)imageReaders.next();
-         
-         else throw new IIOException
-            ("Unsupported image format");
-        
-         FileImageInputStream imageInputStream = new FileImageInputStream(fileImage);
-         
-         imageReader.setInput(imageInputStream);
-         
-         this.width = imageReader.getWidth(0);
-         this.height = imageReader.getHeight(0);
-         
-         BufferedImage bufImage = imageReader.read(0);
-         imageInputStream.close();
-         
-         WritableRaster wRaster = bufImage.getRaster();
-         
-         //complete the remainder of this method
-         this.pixels = new int[this.height][this.width];
-         
-         for (int row = 0; row < height; row++)
-            for (int col = 0; col < width; col++)
-               pixels[row][col] = wRaster.getSample(col,row,0);
-        
-   }
-   
-   
-   public void write(String filename)
-      throws Exception {
-         String ext = filename.substring(filename.indexOf('.')+1);
-         
-         File fileImage = new File(filename);
-         
-         Iterator imageWriters = ImageIO.getImageWritersBySuffix(ext);
-         ImageWriter imageWriter;
-         
-         if (imageWriters.hasNext())
-            imageWriter = (ImageWriter)imageWriters.next();
-            
-         else throw new IIOException
-            ("Unsupported image format");
-         
-         FileImageOutputStream imageOutputStream = new FileImageOutputStream(fileImage);
-         
-         imageWriter.setOutput(imageOutputStream);
-         
-         BufferedImage bufImage = createBufferedImage();
-         
-         imageWriter.write(bufImage);
-         imageOutputStream.close();
-   }
-   
-   
-   public void draw(Graphics gc,int x,int y){
-      BufferedImage bufImage = createBufferedImage();
-      gc.drawImage(bufImage,x,y,null);
-   }
-   
-
-   private BufferedImage createBufferedImage() {
-     
-      BufferedImage bufImage = new BufferedImage(width,height,BufferedImage.TYPE_3BYTE_BGR);
- 
-      WritableRaster wRaster = bufImage.getRaster();
- 
-      for (int row = 0; row < this.height; row++)
-         for (int col = 0; col < this.width; col++){
-         wRaster.setSample(col,row,0,pixels[row][col]);
-         }
-         
-      return bufImage;
-}
-
-   public void flipX() 
+   public void write(String filename) throws Exception 
    {
-      for (int row = 0; row < this.height/2; row++)
-         for (int col = 0; col < this.width; col++) 
-         {
-            int s = pixels[row][col];
-            pixels[row][col] = pixels[this.height-row-1][col];
-            pixels[this.height-row-1][col] = s;
-         }
+        File fileImage = new File(filename);
+        String ext = filename.substring(filename.indexOf('.') + 1);
+        BufferedImage bufImage = createBufferedImage();
+        ImageIO.write(bufImage, ext, fileImage);
    }
    
-   public void chgInt(double pct) 
-   {
-      for (int row = 0; row < height; row++)
-         for (int col = 0; col < width; col++) 
-         {
-            int s = (int)(pixels[row][col] * pct);
-            
-            if (s < 0) 
-               s = WHITE;
-               
-            else if (s > 255) 
-               s = BLACK;
-              
-            pixels[row][col] = s;
-         }
-   }
    
-   public void flipY() 
-   {
-      for (int row = 0; row < this.height; row++)
-         for (int col = 0; col < this.width/2; col++) 
-         {
-            int s = pixels[row][col];
-            pixels[row][col] = pixels[row][this.width-col-1];
-            pixels[row][this.width-col-1] = s;
-         }
-   }
+	public void draw(Graphics gc, int x, int y)
+	{
+   		BufferedImage bufImage = createBufferedImage();
+      	gc.drawImage(bufImage, x, y, null);
+   	}
+
+	// Below methods are to be created by students
+   	public void flipX() 
+   	{
+      	for (int row = 0; row < this.height/2; row++)
+      	{
+         	for (int col = 0; col < this.width; col++) 
+         	{
+            	int s = this.pixels[row][col];
+            	this.pixels[row][col] = this.pixels[this.height - row - 1][col];
+            	this.pixels[this.height - row - 1][col] = s;
+         	}
+        }
+   	}
+   	
+   	public void flipY() 
+   	{
+      	for (int row = 0; row < this.height; row++)
+      	{
+         	for (int col = 0; col < this.width/2; col++) 
+         	{
+            	int s = this.pixels[row][col];
+            	this.pixels[row][col] = this.pixels[row][this.width - col - 1];
+            	this.pixels[row][this.width - col - 1] = s;
+         	}
+        }
+   	}
    
-   public void toBw()
-   {
-      for(int row = 0; row<this.height; row++)
-         for(int col = 0; col<this.width; col++)
-         {
-            if(pixels[row][col] < MID_POINT)
-               pixels[row][col] = WHITE; 
-            else 
-               pixels[row][col] = BLACK; 
-         
-         }
-   }
-   
-  public void rot90()
-  {
-      newPixels = new int [this.height][this.width]; 
-      int count; 
-      
-      for(int row = 0; row<this.width; row++)
-         for(int col = 0; col<this.height; col++)
-         {
-               newPixels[row][col] = pixels[row][col];
-         }
-         
-      for(int row = 0; row<this.width; row++)
-      {
-         count = pixels[row].length-1; 
-         
-         for(int col = 0; col<this.height; col++)
-         {
-               pixels[row][col] = newPixels[count][row];
-               count--; 
-         }
-      }
-      
-  }
- 
-} // End of Class Image
+  
+   	public void toBw()
+   	{
+		for (int row = 0; row < this.height; row++)
+      	{
+         	for (int col = 0; col < this.width; col++)
+         	{
+         		int rgb = this.pixels[row][col];
+            	int r = (rgb >> 16) & 0xFF;
+        		int g = (rgb >> 8) & 0xFF;
+        		int b = (rgb & 0xFF);
+
+        		int grayLevel = (r + g + b) / 3;
+        		int gray = (grayLevel << 16) + (grayLevel << 8) + grayLevel; 
+         		this.pixels[row][col] = gray;
+         	}
+        }
+	}
+} 
--- TestImage.java
+++ TestImage.java
@@ -1,57 +1,40 @@
 import java.awt.*;
 import javax.imageio.*; 
-class TestImage {
 
-   public static void main(String args[])
-      throws Exception {
+class TestImage 
+{
+	public static void main(String args[]) throws Exception 
+	{
+    	Frame frame = new Frame("My Images");
+      	frame.setSize(1024,768);
+      	frame.setVisible(true);
+      	Graphics gc = frame.getGraphics();
       
-      Frame frame = new Frame("Test Image");
-      frame.setSize(1024,768);
-      frame.setVisible(true);
-      Graphics gc = frame.getGraphics();
-       
-      
-      try {
+      	try 
+      	{
+         	//Image img = new Image("animals.jpg");
+         	//img.draw(gc, 10, 40);
          
+         	// Flip the image across y axis
+         	//img.flipY();
+         	//img.draw(gc, 20 + img.getWidth(), 40);
+         	//img.write("AnimalsFlipY.jpg");
+         	
+         
+         	//change image to black and white
+         	/* img.toBw();
+         	img.draw(gc, 20 + img.getWidth(), 40);
+         	img.write("AnimalsBW.jpg"); */
           
-         Image img = new Image("Horse.jpg");
-         
-         img.draw(gc,10,40);
-         /*
-         // Flip the image across y axis
-         img.flipY();
-         //write and draw flipped image
-         img.draw(gc,20+img.getWidth(),40);
-         img.write("HorseFlippedY.jpg");
-         
-         //change image to black and white
-         img.toBw();
-         // Display the black and white image.
-         img.draw(gc,20+img.getWidth(),40);
-         img.write("HorseBW.jpg");
-         
-         //change image intensity by giving percentage
-         img.chgInt(.5); 
-         // Display the black and white image.
-         img.draw(gc,20+img.getWidth(),40);
-         img.write("HorseInt.jpg");
-         
-         //rotate the image 90 degrees clockwise
-         img.rot90(); 
-         img.draw(gc,20+img.getWidth(),40);
-         img.write("HorseRot90.jpg");
-          
-         // Flip the image across y axis
-         img.flipX();
-         //write and draw flipped image
-         img.draw(gc,20+img.getWidth(),40);
-         img.write("HorseFlippedX.jpg");*/
-        
-      } 
-      
-      catch (Exception e) 
-      {
-         System.out.println("Exception in main() "+e.toString());
-      }
-   }
+         	// Flip the image across y axis
+         	/* img.flipX();
+         	img.draw(gc, 20 + img.getWidth(), 40);
+         	img.write("AnimalsFlipX.jpg"); */
+         	
+		} 
+		catch (Exception e)
+		{
+         	e.printStackTrace();
+      	}
+	}
 }
